403e5b70b2572457f31737cbabc091c858d20c5a
Maeriel Benedicto <N00150817@iadt.ie>
2019-09-24 14:13:28 +0100

    my first commit

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+# @Date:   2019-09-17T15:55:29+01:00
+# @Last modified time: 2019-09-24T13:55:55+01:00
+
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/helloworld', function () {
+    return '<h1>Hello World</h1>';
+});
+
+Route::get('/maeriel', function () {
+    return view('maeriel');
+});
